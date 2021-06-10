91a0ff0f386d152bbe48ba954d1cc716c0a18c99
Susanne-Becker <susanne.becker@hva.nl>
2021-06-10 15:14:11 +0200

    dbfiles

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from dbfiles.urls import dbfiles_url
 
 urlpatterns = [
     path("admin/", admin.site.urls),
